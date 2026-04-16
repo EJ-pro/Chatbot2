10c74d864963fb76a3634502aa2717cdd7ae5edc
EJ-pro <hsshss2522@naver.com>
2026-04-16 10:50:43 +0900

    graph 파일 수정

--- common/langgraph/graph.py
+++ common/langgraph/graph.py
@@ -1,4 +1,4 @@
-from langgraph.graph import StateGraph, END
+from langgraph.graph import StateGraph,START, END
 from common.langgraph.nodes import chatbot_node
 from common.langgraph.state import ChatbotState
 
@@ -11,6 +11,7 @@ def create_chatbot_graph():
     # 노드 추가
     graph.add_node("chatbot_node", chatbot_node)
     # 엣지 추가
+    graph.add_edge(START, "chatbot_node")
     graph.add_edge("chatbot_node", END)
     # 그래프 컴파일
     return graph.compile()
